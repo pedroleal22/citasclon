4952b8e1ab67314645e18b54b98c9f5c6a3df1dc
crivilla <celia.rivilla@gmail.com>
2019-11-26 19:21:29 +0100

    Enfermedades CRUD

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -28,7 +28,6 @@ class PacienteController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function create()
